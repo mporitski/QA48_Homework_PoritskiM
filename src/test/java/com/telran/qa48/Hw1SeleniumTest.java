06cb0ea8ffbf463ecfd27333b81a635c442cc14d
Margarita Poritski <mporitski@gmail.com>
2025-08-17 13:41:20 +0300

    changes

--- src/test/java/com/telran/qa48/Hw1SeleniumTest.java
+++ src/test/java/com/telran/qa48/Hw1SeleniumTest.java
@@ -1,4 +1,26 @@
 package com.telran.qa48;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Hw1SeleniumTest {
+
+    WebDriver driver;
+
+    //before    -setUp
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+    }
+
+    //test
+    @Test
+    public void openDemowebshopTest(){
+        System.out.println("Page Demowebshop opened!");
+    }
+
+
 }
